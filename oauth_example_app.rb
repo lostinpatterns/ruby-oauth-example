51148aa8c8bd4d4398970b36bae883118277ad38
Gun.io Whitespace Robot <contact@gun.io>
2011-10-30 23:35:35 -0400

    Remove whitespace [Gun.io WhitespaceBot]

--- oauth_example_app.rb
+++ oauth_example_app.rb
@@ -11,38 +11,38 @@ class OauthExampleApp < Sinatra::Base
 
   get '/' do
     redirect '/authorize' unless session[:access_token]
-    
+
     @user = client.get '/people/me'
-    
+
     erb :index
   end
-  
+
   get '/authorize' do
     redirect Dailymile::Client.oauth_client.web_server.authorize_url(:redirect_uri => redirect_uri, :response_type => 'code')
   end
-  
+
   get '/callback' do
     begin
       access_token = Dailymile::Client.oauth_client.web_server.get_access_token(params[:code], :redirect_uri => redirect_uri, :grant_type => 'authorization_code')
       session[:access_token] = access_token.token
-      
+
       redirect '/'
     rescue
       %{Failed to retrieve access. <a href="/">Try again</a>}
     end
   end
-  
+
 private
-  
+
   def redirect_uri
     uri = URI.parse(request.url)
     "#{uri.scheme}://#{uri.host}:#{uri.port}/callback"
   end
-  
+
   def client
     @client ||= Dailymile::Client.new(session[:access_token])
   end
-  
+
 end
 
 OauthExampleApp.run!
